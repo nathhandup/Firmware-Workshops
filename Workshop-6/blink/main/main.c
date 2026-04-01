be40528109010388ab1edc5f2d3e763a7c035166
nathan <natedup8@gmail.com>
2026-04-01 16:19:58 -0400

    Workshop-6 blink sample project

--- Workshop-6/blink/main/main.c
+++ Workshop-6/blink/main/main.c
@@ -1,11 +1,3 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -29,6 +21,7 @@ static void blink_led(void)
 static void configure_led(void)
 {
     gpio_reset_pin(BLINK_GPIO);
+    
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
